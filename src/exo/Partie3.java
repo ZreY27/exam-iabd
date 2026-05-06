b844860befe58a516f2b5b46ec38bc623afd56ca
antoine <a.reelitin@gmail.com>
2026-05-06 15:07:18 +0200

    :construction: Partie3.java

--- src/exo/Partie3.java
+++ src/exo/Partie3.java
@@ -2,18 +2,27 @@ package exo;
 
 import models.Trip;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class Partie3 {
 
+    // Exercice 9: Créer les comparators
+    Comparator<Trip> byPrice = Comparator.comparing(Trip::price);
+    Comparator<Trip> byRating = Comparator.comparing(Trip::rating);
+
     public List<Trip> top10ExpensiveTrips(List<Trip> trips) {
         // coder ici
-        return List.of();
+        return trips.stream()
+                .sorted(byPrice.reversed()) // Tri par prix, ordre décroissant
+                .limit(10)
+                .toList();
     }
 
     public Optional<Trip> bestTrip(List<Trip> trips) {
         // coder ici
-        return Optional.empty();
+        return trips.stream()
+                .max(byRating); // Trouve le trajet avec la note maximale
     }
 }
